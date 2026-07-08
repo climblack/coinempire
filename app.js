b4dca74976cbd6c2a0b773f6c983cefdd3917206
Climblack <black.wolf.tv11@gmail.com>
2026-07-08 10:31:33 +0300

    Set up Express server with CORS and JSON support

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const cors = require("cors");
 
+const db = require("./database");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+
+    res.send("CoinEmpire API работает");
+
+});
+
+app.listen(3000,()=>{
+
+    console.log("Server started");
+
+});
